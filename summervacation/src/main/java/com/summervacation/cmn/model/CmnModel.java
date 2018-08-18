6ac460d0596adee7da2839eab607420b7b86c145
vndrh <vndrh@hmkim-pc>
2018-08-18 16:27:07 +0900

    모델커밋

---
+++ summervacation/src/main/java/com/summervacation/cmn/model/CmnModel.java
@@ -0,0 +1,11 @@
+package com.summervacation.cmn.model;
+
+import lombok.Data;
+
+@Data
+public class CmnModel {
+	
+	private String sccYn;
+	private String resultMsg;
+
+}
---
+++ summervacation/src/main/java/com/summervacation/cmn/model/TB0000.java
@@ -0,0 +1,24 @@
+package com.summervacation.cmn.model;
+
+import lombok.Data;
+
+@Data
+public class TB0000 extends CmnModel{
+
+	private String empNo;
+	private String loginId;
+	private String loginPwd;
+	private String userNm;
+	private String frontRegNo;
+	private String gender;
+	private String marrYn;
+	private String telNum;
+	private String email;
+	private String deptCd;
+	private String gradeCd;
+	private String fstRegDttm;
+	private String fstRgstEno;
+	private String lstChgDttm;
+	private String lstChgEno;
+	
+}
